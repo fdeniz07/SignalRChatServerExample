a6953c4340d66181cb98cd500c4bfef026602b7b
Fatih Deniz <deniz.fatih@hotmail.de>
2023-01-07 06:35:21 +0100

    Add project files.

---
+++ SignalRChatServerExample/Hubs/ChatHub.cs
@@ -0,0 +1,8 @@
+﻿namespace SignalRChatServerExample.Hubs
+{
+    using Microsoft.AspNetCore.SignalR;
+
+    public class ChatHub:Hub
+    {
+    }
+}
---
+++ SignalRChatServerExample/Program.cs
@@ -0,0 +1,38 @@
+using SignalRChatServerExample.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddCors(option => option.AddDefaultPolicy(policy => policy
+        .AllowCredentials()
+        .AllowAnyHeader()
+        .AllowAnyHeader()
+        .SetIsOriginAllowed(x=>true)));
+
+builder.Services.AddSignalR();
+
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseCors();
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapHub<ChatHub>("/chatHub");
+    endpoints.MapControllers();
+});
+
+
+//app.MapGet("/", () => "Hello World!");
+
+app.Run();
