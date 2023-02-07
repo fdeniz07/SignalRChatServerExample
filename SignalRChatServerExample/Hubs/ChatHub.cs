264187673e5f9f99b90d937a28218117ed55dfc0
Fatih Deniz <deniz.fatih@hotmail.de>
2023-02-07 23:51:21 +0100

    Server

--- SignalRChatServerExample/Hubs/ChatHub.cs
+++ SignalRChatServerExample/Hubs/ChatHub.cs
@@ -4,7 +4,7 @@
     using Microsoft.AspNetCore.SignalR;
     using Models;
 
-    public class ChatHub:Hub
+    public class ChatHub : Hub
     {
         public async Task GetNickName(string nickName)
         {
@@ -16,6 +16,20 @@
             ClientSource.Clients.Add(client);
             await Clients.Others.SendAsync("clientJoined", nickName);
             await Clients.All.SendAsync("clients", ClientSource.Clients);
+        }
+
+        public async Task SendMessageAsync(string message, string clientName)
+        {
+            if (clientName =="Tümü")
+            {
+                await Clients.All.SendAsync("receiveMessage", message);
+            }
+            else
+            {
+               Client client = ClientSource.Clients.FirstOrDefault(c => c.NickName == clientName);
+               await Clients.Client(client.ConnectionId).SendAsync("receiveMessage", message);
+            }
+           
         }
     }
 }
